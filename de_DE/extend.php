5278fd136dc00e47f4120aecec9e9f55aefc2207
lukas-bold <lukas@transparentcast.de>
2013-08-31 23:33:13 +0200

    new file:   de_DE/extend.php 	new file:   de_DE/global.php 	new file:   de_DE/menu.php 	modified:   de_DE/plugins.php

---
+++ de_DE/extend.php
@@ -0,0 +1,67 @@
+<?php
+
+return array(
+
+	'extend' => 'Erweitern',
+
+	'fields' => 'Benutzerdefinierte Felder',
+	'fields_desc' => 'Zusätzliche Felder hinzufügen',
+
+	'variables' => 'Seiten-Variablen',
+	'variables_desc' => 'Zusätzliche Metadaten hinzufügen',
+
+	'create_field' => 'Neues Feld erstellen',
+	'editing_custom_field' => 'Bearbeite Feld &ldquo;%s&rdquo;',
+	'nofields_desc' => 'Noch keine Felder',
+
+	'create_variable' => 'Create a new variable',
+	'editing_variable' => 'Editing variable &ldquo;%s&rdquo;',
+	'novars_desc' => 'No variables yet',
+
+	// form fields
+	'type' => 'Typ',
+	'type_explain' => 'Der Typ der Inhalte, die zu diesem Feld hinzugefügt werden sollen.',
+
+	'field' => 'Feld',
+	'field_explain' => 'HTML input Typ',
+
+	'key' => 'Schlüsselwort',
+	'key_explain' => 'Eindeutiges Schlüsselwort für das Feld',
+	'key_missing' => 'Füge ein Schlüsselwort hinzu.',
+	'key_exists' => 'Schlüsselwort wird bereits benutzt.',
+
+	'label' => 'Beschriftung',
+	'label_explain' => 'Allgemein verständlicher Name für das Feld',
+	'label_missing' => 'Gib eine Beschriftung an.',
+
+	'attribute_type' => 'Dateityp',
+	'attribute_type_explain' => 'Durch Kommas getrennte Liste von akzeptierten Dateitypen. Leer lassen, um alle zu akzeptieren.',
+
+	// images
+	'attributes_size_width' => 'Maximalbreite',
+	'attributes_size_width_explain' => 'Bilder werden verkleinert, wenn sie die Maximalgröße überschreiten.',
+
+	'attributes_size_height' => 'Maximalhöhe',
+	'attributes_size_height_explain' => 'Bilder werden verkleinert, wenn sie die Maximalgröße überschreiten.',
+
+	// custom vars
+	'name' => 'Name',
+	'name_explain' => 'Eindeutig Name',
+	'name_missing' => 'Gib einen eindeutigen Namen an.',
+	'name_exists' => 'Name existiert bereits',
+
+	'value' => 'Wert',
+	'value_explain' => 'Daten, die Du speichern willst. (bis zu 64kb)',
+	'value_code_snipet' => 'Code zum Einfügen in dein Template:<br>
+		<code>' . e('<?php echo site_meta(\'%s\'); ?>') . '</code>',
+
+	// messages
+	'variable_created' => 'Deine Variable wurde erstellt',
+	'variable_updated' => 'Deine Variable wurde aktualisiert',
+	'variable_deleted' => 'Deine Variable wurde gelöscht',
+
+	'field_created' => 'Dein Feld wurde erstellt',
+	'field_updated' => 'Dein Feld wurde aktualisiert',
+	'field_deleted' => 'Dein Feld wurde gelöscht',
+
+);
