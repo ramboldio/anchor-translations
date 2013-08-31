fef0b6c97b05d47d785007ffe9555dca8d925068
lukas-bold <lukas@transparentcast.de>
2013-08-31 22:45:09 +0200

    Added some German translations

---
+++ de_DE/global.php
@@ -0,0 +1,61 @@
+<?php
+
+/*
+ * Global is to replace common.php
+ *
+ * This global file will provide the majority of the translations in anchor
+ * to remove duplicate translations in section specific language files.
+ */
+return array(
+
+	// words
+	'save' => 'Speichern',
+	'delete' => 'Löschen',
+	'update' => 'Update',
+	'edit' => 'Bearbeiten',
+	'editing' => 'Editing',
+	'create' => 'Create',
+	'created' => 'Created',
+	'submit' => 'Senden',
+	'close' => 'Schließen',
+	'status' => 'Status',
+	'manage' => 'Verwalten',
+	'reset' => 'Zurücksetzen',
+	'all' => 'Alles',
+
+	// pagination
+	'next' => 'Nächstes', // -r Blogeintrag?
+	'previous' => 'Vorheriges',
+	'first' => 'Erstes',
+	'last' => 'Letztes',
+
+	// statuses
+	'draft' => 'Entwurf',
+	'archived' => 'Archiviet',
+	'published' => 'Veröffentlicht',
+	'pending' => 'Ausstehend',
+	'approved' => 'Bestätigt',
+	'spam' => 'Spam',
+
+	'inactive' => 'Inaktiv',
+	'active' => 'Aktiv',
+
+	// roles
+	'administrator' => 'Admin',
+	'editor' => 'Autor', // Redakteur?
+	'user' => 'Benutzer',
+
+	'log_in' => 'Einloggen', // verb
+	'login' => 'Einloggen', // noun
+	'log_out' => 'Ausloggen',
+	'logout' => 'Ausloggen',
+
+	// pharses
+	'visit_your_site' => 'Besuche Deine Seite',
+	'powered_by_anchor' => 'Powered by Anchor, version %s',
+	'make_blogging_beautiful' => 'Mach Bloggen wunderschön.',
+
+	// messages
+	'confirm_delete' => 'Bist du sicher, dass Du das löschen willst? Dies kann nicht rückgängig gemacht werden.'
+
+);
---
+++ de_DE/plugins.php
@@ -0,0 +1,12 @@
+<?php
+
+return array(
+
+	'plugins' => 'Plugins',
+	'plugins_desc' => 'Verwalte deine Plugins',
+	'noplugins_desc' => 'Get Coding',
+
+	// messages
+	'installed' => '%s wurden installiert',
+	'uninstalled' => '%s wurden deinstalliert'
+);
